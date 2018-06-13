dcf12d1813e20c0ad204449c6f239430396561de
Daniel Carvalho <danielbpc2@hotmail.com>
2018-06-13 14:04:10 -0700

    outputs in json format now in wallet central

--- lib/wallet.rb
+++ lib/wallet.rb
@@ -1,6 +1,6 @@
 require 'csv'
 require 'json'
-require 'pry'
+
   # all wallets array
   WALLETS = []
 
@@ -24,33 +24,6 @@ class Wallet
     csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
     CSV.foreach(filepath, csv_options) do |row|
     Wallet.new(row['Client'], row['Currency'], row['Amount'])
-    end
-  end
-
-  def self.json_output
-    # create a json filepath
-    filepath = 'resources/wallets_output.json'
-    # wallets that will be written in json format
-    output_wallets = []
-
-    WALLETS.each_with_index do |wallet, index|
-     # separate the wallets with the same client as the one in the current loop
-      same_name = WALLETS.select { |x| wallet.client == x.client }
-      # save the client name in the output and create a hash wallets
-      output_wallets << {
-                            name: "#{wallet.client}",
-                            "wallets": {}
-                          }
-      # grab the wallets that has the same client and store their wallets in the empty hash
-      same_name.each do |x|
-        output_wallets[index][:wallets].store("#{x.currency}", "#{x.amount}")
-      end
-    end
-    # remove duplicates in the output
-    output_wallets.uniq!
-    # generate json
-    File.open(filepath, 'wb') do |file|
-      file.write(JSON.generate(output_wallets))
     end
   end
 
--- lib/wallet_central.rb
+++ lib/wallet_central.rb
@@ -7,7 +7,68 @@ class WalletCentral
   end
 
   def transfer(wallet1, wallet2, currency, amount_transfered)
+
+  end
+
+  def output(client)
+    # wallets that will be written in json format
+    output_wallet = []
+
+    WALLETS.each_with_index do |wallet, index|
+     # separate the wallets with the same client as the one in the current loop
+      same_name = WALLETS.select { |x| wallet.client == x.client }
+      # save the client name in the output and create a hash wallets
+      output_wallet << {
+                            name: "#{wallet.client}",
+                            "wallets": {}
+                          }
+      # grab the wallets that has the same client and store their wallets in the empty hash
+      same_name.each do |x|
+        output_wallet[index][:wallets].store("#{x.currency}", "#{x.amount}")
+      end
+    end
+    # remove duplicates in the output
+    output_wallet.uniq!
+    # return asked client
+    output_wallet.select! {|c| c[:name] == client}
+
+    unless output_wallet.length == 0
+      return output_wallet
+    else
+      return "No client with such name"
+    end
+  end
+
+private
+
+  def all_output
+    # create a json filepath
+    filepath = 'resources/wallets_output.json'
+    # wallets that will be written in json format
+    output_wallets = []
+
+    WALLETS.each_with_index do |wallet, index|
+     # separate the wallets with the same client as the one in the current loop
+      same_name = WALLETS.select { |x| wallet.client == x.client }
+      # save the client name in the output and create a hash wallets
+      output_wallets << {
+                            name: "#{wallet.client}",
+                            "wallets": {}
+                          }
+      # grab the wallets that has the same client and store their wallets in the empty hash
+      same_name.each do |x|
+        output_wallets[index][:wallets].store("#{x.currency}", "#{x.amount}")
+      end
+    end
+    # remove duplicates in the output
+    output_wallets.uniq!
+    # generate json
+    File.open(filepath, 'wb') do |file|
+      file.write(JSON.generate(output_wallets))
+    end
   end
 
 end
 
+a = WalletCentral.new
+puts a.output('jon')
